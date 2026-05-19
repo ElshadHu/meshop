b5140d83571fdce6fa70a2768478515bbfa730b0
ElshadHu <elsadhumbetli079@gmail.com>
2026-05-19 17:27:26 -0400

    chore: create a logger bridge

---
+++ mobile/libmeshop/logger.go
@@ -0,0 +1,30 @@
+package libmeshop
+
+import (
+	"io"
+	"log/slog"
+	"strings"
+)
+
+// slogFromLogger returns a slog.Logger whose output is forwarded to the Java-side logger
+func slogFromLogger(log Logger) *slog.Logger {
+	if log == nil {
+		return slog.New(slog.NewTextHandler(io.Discard, nil))
+	}
+	return slog.New(slog.NewTextHandler(&loggerWriter{log: log}, nil))
+}
+
+type loggerWriter struct {
+	log Logger
+}
+
+func (w *loggerWriter) Write(p []byte) (int, error) {
+	w.log.Log(strings.TrimRight(string(p), "\n"))
+	return len(p), nil
+}
+
+var _ io.Writer = (*loggerWriter)(nil)
+
+func _newSilentLogger() *slog.Logger {
+	return slog.New(slog.NewTextHandler(io.Discard, &slog.HandlerOptions{}))
+}
